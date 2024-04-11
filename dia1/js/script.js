53762d365f20fa2bfcb2da2a54f4d3d438929bc5
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-04-11 08:02:04 -0500

    feat: :sparkles: practica de variables
    practica de variables

--- dia1/js/script.js
+++ dia1/js/script.js
@@ -1,1 +1,12 @@
 console.log("Hola mundo!!");
+a= 5
+b=6
+console.log(a*b);
+
+operacion = (((((5**8))+5)));
+console.log(operacion);
+
+txt1="Campus"
+txt2="Land";
+concatenacion=txt1+txt2;
+console.log(concatenacion)

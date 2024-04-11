c4ec6cb5d2f8d52e8a2e1a19ea055c0b2f9bdb68
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-04-11 08:52:39 -0500

    feat: :sparkles: practica de ejercicio funcion
    se logro hacer una funcion para celsius a farenhei

--- dia1/js/script.js
+++ dia1/js/script.js
@@ -10,29 +10,30 @@ txt1="Campus"
 txt2="Land";
 concatenacion=txt1+txt2;
 console.log(concatenacion)
-// ******************Funcion sin retorno y sin parametro*************
-function funcionNormal(){
-    // console.log("Mi funcion");
 
-};
-// ******************Funcion sin retorno y con parametro*************
+// // ******************Funcion sin retorno y sin parametro*************
+// function funcionNormal(){
+//     // console.log("Mi funcion");
 
-function suma(a,b){
-    // console.log(a+b);
-}
-suma(7,6)
-// ******************Funcion con retorno y con parametro*************
-function sumaR(a,b){
-    //console.log(a+b);
-    // return a+b
-}
-suma(6,6)
-console.log(sumaR(5,8) + sumaR(5,12))
+// };
+// // ******************Funcion sin retorno y con parametro*************
+
+// function suma(a,b){
+//     // console.log(a+b);
+// }
+// suma(7,6)
+// // ******************Funcion con retorno y con parametro*************
+// function sumaR(a,b){
+//     //console.log(a+b);
+//     // return a+b
+// }
+// suma(6,6)
+// console.log(sumaR(5,8) + sumaR(5,12))
 // ******************Funcion con retorno y sin parametro*************
-function salonfavorito(){
-    //console.log(a+b)
-    // return "P1";
-}
+// function salonfavorito(){
+//     //console.log(a+b)
+//     // return "P1";
+// }
 //*********************** BUCLE FOR ************************
 //i+ de uno en uno o cualquier numero
 arreglo=[123,"pedrfo",true]
@@ -42,3 +43,9 @@ tamano=arreglo.length;
 for(let i=0;i<tamano;i++){
     console.log(arreglo[i])
 }
+
+function gradosC(c){
+    formula=(9*c/5)+32
+    return formula
+}
+console.log(gradosC(25));

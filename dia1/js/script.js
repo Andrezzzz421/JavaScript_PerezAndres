7f775992ab2a6f0150c98bd3d8dc45f3f851884a
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-04-11 08:37:18 -0500

    feat: :sparkles: practica for e arreglos
    practica de bluce for e arrays

--- dia1/js/script.js
+++ dia1/js/script.js
@@ -12,25 +12,33 @@ concatenacion=txt1+txt2;
 console.log(concatenacion)
 // ******************Funcion sin retorno y sin parametro*************
 function funcionNormal(){
-    console.log("Mi funcion");
+    // console.log("Mi funcion");
 
 };
 // ******************Funcion sin retorno y con parametro*************
 
 function suma(a,b){
-    console.log(a+b);
+    // console.log(a+b);
 }
 suma(7,6)
 // ******************Funcion con retorno y con parametro*************
 function sumaR(a,b){
     //console.log(a+b);
-    return a+b
+    // return a+b
 }
 suma(6,6)
 console.log(sumaR(5,8) + sumaR(5,12))
 // ******************Funcion con retorno y sin parametro*************
 function salonfavorito(){
     //console.log(a+b)
-    return "P1";
+    // return "P1";
 }
+//*********************** BUCLE FOR ************************
+//i+ de uno en uno o cualquier numero
+arreglo=[123,"pedrfo",true]
+console.log(arreglo.length);
 
+tamano=arreglo.length;
+for(let i=0;i<tamano;i++){
+    console.log(arreglo[i])
+}

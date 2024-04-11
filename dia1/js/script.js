4bc283af3489bbedc65ec03c878d87a4417aeea6
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-04-11 08:01:18 -0500

    feat: :sparkles: practica de variables
    practica de variables

--- dia1/js/script.js
+++ dia1/js/script.js
@@ -10,3 +10,26 @@ txt1="Campus"
 txt2="Land";
 concatenacion=txt1+txt2;
 console.log(concatenacion)
+// ******************Funcion sin retorno y sin parametro*************
+function funcionNormal(){
+    console.log("Mi funcion");
+
+};
+// ******************Funcion sin retorno y con parametro*************
+
+function suma(a,b){
+    console.log(a+b);
+}
+suma(7,6)
+// ******************Funcion con retorno y con parametro*************
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b
+}
+suma(6,6)
+console.log(sumaR(5,8) + sumaR(5,12))
+// ******************Funcion con retorno y sin parametro*************
+function salonfavorito(){
+    //console.log(a+b)
+    return "P1";
+}
